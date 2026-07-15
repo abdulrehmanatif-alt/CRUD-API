de24a9d7d9301ee8be017b70a909c0359fb11aa0
abdulrehmanatif <abdulrehmanatif51@gmail.com>
2026-07-15 16:21:20 +0500

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 class TaskCreate(BaseModel):
     title: str
 
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
 tasks = [
     {
         "id": 1,
@@ -74,3 +78,36 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+
+    if not updated_task.title.strip():
+        raise HTTPException(
+            status_code=400,
+            detail="Title cannot be empty."
+        )
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
